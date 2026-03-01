a4fd50fd81666e38e1a533d3cba8e189c1d7c283
simple9920 <simple9920@gmail.com>
2026-03-01 22:43:29 +0900

    day22更新

---
+++ day22_dictionary_practice.py
@@ -0,0 +1,16 @@
+#書き換えを伴う処理
+#1
+matrix = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 0, 1]
+]
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix != 0:#ここはmatix[i][j]  == 0:
+            matrix[i][j] = 1
+
+print(matrix)
+
+#
